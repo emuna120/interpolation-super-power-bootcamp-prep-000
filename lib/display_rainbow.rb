69b1be4677a34dcf7fb04caf1f4a5bbd0eaceb1c
Amy Man <jwb74fko3ovz@beconfidential.com>
2019-02-04 02:02:30 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -7,4 +7,5 @@ def display_rainbow(colors)
   print "#{colors[4][0].upcase}: #{colors[4]}, "
   print "#{colors[5][0].upcase}: #{colors[5]}, "
   print "#{colors[6][0].upcase}: #{colors[6]}"
+  puts
 end  
